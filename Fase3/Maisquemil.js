85fdd210c7bc26df757e361dc603a5a337d196cd
jennyfferbrand <81859975+jennyfferbrand@users.noreply.github.com>
2021-04-05 01:16:24 -0300

    Create Maisquemil.js

---
+++ Fase3/Maisquemil.js
@@ -0,0 +1,9 @@
+//O sistema MAIS QUE MIL apresenta o resultado da soma dos 
+//4 primeiros múltiplicos de 4 acima de mil, subtraindo dos 4 
+//primeiros números primos a partir do zero.
+
+  var multiplicosde4 = 1004 + 1008 + 1012 + 1016;
+  var primos = 2 + 3 + 5 + 7; 
+  var resultado = multiplicosde4 - primos;
+
+console.log(" O resultado é " +resultado+ ".");
