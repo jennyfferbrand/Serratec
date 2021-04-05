900f35dbb9e969026189aead75da3fefd8dd5779
jennyfferbrand <81859975+jennyfferbrand@users.noreply.github.com>
2021-04-05 01:27:58 -0300

    Create Tempodevida.js

---
+++ Fase3/Tempodevida.js
@@ -0,0 +1,12 @@
+// Soma das idades dos integrantes do Squad.
+
+var alice = 18;
+var giovanne = 27;
+var giulia = 22; 
+var larissa = 23;
+var taiana = 26;
+var jennyffer = 20;
+
+var total = alice + giovanne + giulia + larissa + taiana + jennyffer
+
+console.log(" A soma das idades dos integrantes do Squad é " +total+ ".")
