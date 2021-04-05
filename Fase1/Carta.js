e43a04ee8e597f66bf841e2623945337950d9bef
jennyfferbrand <81859975+jennyfferbrand@users.noreply.github.com>
2021-04-04 22:51:21 -0300

    Create Carta.js

---
+++ Fase1/Carta.js
@@ -0,0 +1,8 @@
+{
+    console.log("Carta para Jennyffer do futuro");
+    console.log("Querida, jennyffer!");
+    console.log("  Esquero que você tenha aproveitado com muita sabedoria essa oportunidade! E que em meio a tantas dificuldades você se torne uma pessoa forte, pronta para alcançar novos desafios. ");
+    console.log(" ");
+    console.log("");
+    console.log("");
+}
