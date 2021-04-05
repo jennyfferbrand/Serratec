974836141d1272225a57b826497612125d5dd3ee
jennyfferbrand <81859975+jennyfferbrand@users.noreply.github.com>
2021-04-04 23:16:57 -0300

    Update Carta.js

--- Fase1/Carta.js
+++ Fase1/Carta.js
@@ -3,5 +3,5 @@
     console.log("Querida, jennyffer!");
     console.log("  Sabemos o quanto pode ser dificil diante diversos motivos, mas eu sei que você vai por tudo isso com muita garra e empolgação,e que em meio a tantas dificuldades você se torne uma pessoa forte, pronta para alcançar novos desafios. ");
     console.log("  Esse curso irá trazer muitos conhecimentos e você vai aproveitar com muita sabedoria essa oportunidade! ");
-    console.log("");
+    console.log("Gratidão!! Ass Jennyffer");
 }
