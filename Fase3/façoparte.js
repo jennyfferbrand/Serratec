0786d0ca4b761c297e813abe2a350917a552fc90
jennyfferbrand <81859975+jennyfferbrand@users.noreply.github.com>
2021-04-05 07:26:56 -0300

    Create façoparte.js

---
+++ Fase3/façoparte.js
@@ -0,0 +1,14 @@
+/* O sistema faço parte vaii te mostrar um histórico da trajetória da nosso cidade.
+   O resultado será: Teresópolis tem X anos. Desses y antes de mim. Mas nos ultimos
+   z anos ele conta comigo! 
+*/
+
+var x= 129;
+var z= 20;
+var y= x - z
+
+
+console.log("Teresópolis tem" ,x, "anos!");
+console.log("Desses " ,y, "antes de mim.");
+console.log("Mas nos útimos " ,y, "ela conta comigo!");
+
