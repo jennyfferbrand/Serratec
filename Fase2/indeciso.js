af20370afaebd926d442df67a46ff7c6a14ea231
jennyfferbrand <81859975+jennyfferbrand@users.noreply.github.com>
2021-04-05 00:51:36 -0300

    Create indeciso.js

---
+++ Fase2/indeciso.js
@@ -0,0 +1,9 @@
+// Indeciso
+  var numero1 = 13;
+  var numero2 = 7;
+  var numero3 = 22;
+  var numero4 = 20;
+
+  console.log("Quero te contar meu número da sorte! é " +numero1+ ".");
+  console.log("Não, espera, é " +numero2+ ".. Ah esse é da mamãe. O meu é " +numero3+ ".");
+  console.log("Já decidi: é " +numero4+ "! Bindo :)");
