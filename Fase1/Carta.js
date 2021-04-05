534bbe89b49bd881e617173f27803dc1b7a11ba9
jennyfferbrand <81859975+jennyfferbrand@users.noreply.github.com>
2021-04-04 22:59:37 -0300

    Update Carta.js

--- Fase1/Carta.js
+++ Fase1/Carta.js
@@ -1,8 +1,7 @@
 {
     console.log("Carta para Jennyffer do futuro");
     console.log("Querida, jennyffer!");
-    console.log("  Esquero que você tenha aproveitado com muita sabedoria essa oportunidade! E que em meio a tantas dificuldades você se torne uma pessoa forte, pronta para alcançar novos desafios. ");
-    console.log(" ");
-    console.log("");
+    console.log("  Sabemos o quanto pode ser dificil diante diversos motivos, mas eu sei que você passou por tudo isso com muito garra e empolgação,e que em meio a tantas dificuldades você se tornou uma pessoa forte, pronta para alcançar novos desafios. ");
+    console.log("  Esquero que você tenha aproveitado com muita sabedoria essa oportunidade!");
     console.log("");
 }
