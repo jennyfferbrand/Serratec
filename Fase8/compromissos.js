3aaec97de7c1a2cf82ac1f5d1a8184f0e110315b
jennyfferbrand <81859975+jennyfferbrand@users.noreply.github.com>
2021-04-19 21:35:56 -0300

    Create compromissos.js

---
+++ Fase8/compromissos.js
@@ -0,0 +1,22 @@
+const ler = require("prompt-sync")();
+
+var dia = [];
+do{
+var diaCompromisso =  Number( ler("Que dia do mês deseja agendar um compromisso? "));
+var jaTem = "";
+ 
+    for (let index = 0; index < dia.length; index++) {
+        if ( dia[index] == diaCompromisso ){
+            jaTem = true;
+        } 
+    }
+    if (jaTem == true){
+        var condicao = ler("Esse dia já possui compromisso, deseja agendar outro dia?")
+    }else{
+        dia.push(diaCompromisso);
+        let compromisso= ler("Qual compromisso deseja agendar no dia " + diaCompromisso +"?");
+        console.log("Seu compromisso: " + compromisso + ", foi agendado com sucesso!");
+        condicao =  ler("Se deseja agendar mais algum compromisso digite (sim): ");
+    }
+
+} while(condicao == "sim" )
